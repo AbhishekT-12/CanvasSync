b943fda9b6e54c8fb51861dc96799dbbb3c93471
Ardraanair <ardranairarun@gmail.com>
2026-06-11 19:39:37 +0530

    Add canvas frontend and local drawing functionality

---
+++ static/script.js
@@ -0,0 +1,30 @@
+const canvas = document.getElementById("whiteboard");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let drawing = false;
+
+canvas.addEventListener("mousedown", () => {
+    drawing = true;
+});
+
+canvas.addEventListener("mouseup", () => {
+    drawing = false;
+    ctx.beginPath();
+});
+
+canvas.addEventListener("mousemove", draw);
+
+function draw(event) {
+    if (!drawing) return;
+
+    ctx.lineWidth = 3;
+    ctx.lineCap = "round";
+
+    ctx.lineTo(event.clientX, event.clientY);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(event.clientX, event.clientY);
+}
